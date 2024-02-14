f55a28c9d360d3205723d09ad39d9eac55ba641c
Michael Harris <mharris@luabase.com>
2024-02-14 12:13:20 -0500

    feat: add xplr config

---
+++ xplr/init.lua
@@ -0,0 +1,37 @@
+version = '0.21.5'
+
+xplr.config.general.show_hidden = true
+
+local home = os.getenv("HOME")
+local xpm_path = home .. "/.local/share/xplr/dtomvan/xpm.xplr"
+local xpm_url = "https://github.com/dtomvan/xpm.xplr"
+
+package.path = package.path
+  .. ";"
+  .. xpm_path
+  .. "/?.lua;"
+  .. xpm_path
+  .. "/?/init.lua"
+
+os.execute(
+  string.format(
+    "[ -e '%s' ] || git clone '%s' '%s'",
+    xpm_path,
+    xpm_url,
+    xpm_path
+  )
+)
+
+require("xpm").setup({
+  plugins = {
+    -- Let xpm manage itself
+    'dtomvan/xpm.xplr',
+    { name = 'sayanarijit/fzf.xplr' },
+    { name = 'sayanarijit/tree-view.xplr' },
+  },
+  auto_install = true,
+  auto_cleanup = true,
+})
+
+require("fzf").setup()
+require("tree-view").setup()

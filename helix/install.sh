d365e5c41ec1b24069571c9dfa20e96c08506879
Michael Harris <mharris@luabase.com>
2024-03-05 10:17:17 -0500

    feat(hx): refactor install script

--- helix/install.sh
+++ helix/install.sh
@@ -1,72 +1,56 @@
-#!/bin/bash
+#!/opt/homebrew/bin/bash
 
-NO_COLOR='\033[0m'
 GREEN='\033[0;32m'
+NO_COLOR='\033[0m'
 
-echo -e "[${GREEN}INFO${NO_COLOR}] Installing dependencies"
+info() {
+  echo -e "[${GREEN}INFO${NO_COLOR}] $1"
+}
 
-# Bash
-echo -e "[${GREEN}INFO${NO_COLOR}] bash"
-npm i -g bash-language-server --silent
+declare -A npm_packages=(
+  [bash]="bash-language-server"
+  [docker]="dockerfile-language-server-nodejs"
+  [web]="vscode-langservers-extracted typescript typescript-language-server prettier"
+  [python]="pyright"
+  [tailwindcss]="@tailwindcss/language-server"
+  [vue]="@vue/language-server"
+  [yaml]="yaml-language-server"
+)
 
-# CSS
-echo -e "[${GREEN}INFO${NO_COLOR}] css"
-npm i -g vscode-langservers-extracted --silent
+declare -A brew_packages=(
+  [bash]="shfmt"
+  [lua]="lua-language-server"
+  [markdown]="marksman"
+  [rust]="llvm"
+)
 
-# Docker
-echo -e "[${GREEN}INFO${NO_COLOR}] docker"
-npm install -g dockerfile-language-server-nodejs --silent
+declare -A cargo_crates=(
+  [markdown]="--git https://github.com/github-language-server/github-lsp"
+  [toml]="taplo-cli --locked --features lsp"
+)
 
-# HTML
-echo -e "[${GREEN}INFO${NO_COLOR}] html"
-npm i -g vscode-langservers-extracted --silent
+declare -A pypi_packages=(
+  [python]="ruff-lsp black"
+)
 
-# JSON
-echo -e "[${GREEN}INFO${NO_COLOR}] json"
-npm i -g vscode-langservers-extracted --silent
+info "Installing dependencies"
 
-# JS/TS
-echo -e "[${GREEN}INFO${NO_COLOR}] js/ts"
-npm install -g typescript typescript-language-server --silent
-npm i -g prettier --silent
+for lang in "${!npm_packages[@]}"; do
+  info "Installing npm packages for $lang"
+  npm i -g ${npm_packages[$lang]} --silent
+done
 
-# Lua
-echo -e "[${GREEN}INFO${NO_COLOR}] lua"
-brew install lua-language-server
+for lang in "${!brew_packages[@]}"; do
+  info "Installing brew packages for $lang"
+  brew install ${brew_packages[$lang]} --quiet
+done
 
-# Markdown
-echo -e "[${GREEN}INFO${NO_COLOR}] markdown"
-brew install marksman --quiet
-cargo install --git https://github.com/github-language-server/github-lsp --quiet
+for lang in "${!cargo_crates[@]}"; do
+  info "Installing cargo crates for $lang"
+  cargo install ${cargo_crates[$lang]} --quiet
+done
 
-# Python
-echo -e "[${GREEN}INFO${NO_COLOR}] python"
-npm install pyright -g --silent
-pipx install ruff-lsp
-pipx install black
-
-# Rust
-echo -e "[${GREEN}INFO${NO_COLOR}] rust"
-rustup component add rust-analyzer
-brew install llvm --quiet
-ln -s $(brew --prefix)/opt/llvm/bin/lldb-vscode $(brew --prefix)/bin/
-
-# Tailwindcss
-echo -e "[${GREEN}INFO${NO_COLOR}] tailwindcss"
-npm install @tailwindcss/language-server -g --silent
-
-# TOML
-echo -e "[${GREEN}INFO${NO_COLOR}] toml"
-cargo install taplo-cli --locked --features lsp --quiet
-
-# Vue
-echo -e "[${GREEN}INFO${NO_COLOR}] vue"
-npm install @vue/language-server -g --silent
-
-# YAML
-echo -e "[${GREEN}INFO${NO_COLOR}] yaml"
-npm install yaml-language-server -g --silent
-
-# health check
-echo -e "[${GREEN}INFO${NO_COLOR}] 🧬 health"
-hx --health
+for lang in "${!pypi_packages[@]}"; do
+  info "Installing pypi packages for $lang"
+  pipx install ${pypi_packages[$lang]} --quiet
+done
